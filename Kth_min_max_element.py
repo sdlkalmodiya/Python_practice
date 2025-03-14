d3c6ea0087eb5cbce6f25fa46e488728e9036e53
sdlkalmodiya <101276375+sdlkalmodiya@users.noreply.github.com>
2025-03-14 21:45:55 +0530

    with duplicates in the array

--- Kth_min_max_element.py
+++ Kth_min_max_element.py
@@ -7,3 +7,19 @@ k_min = arr1[k-1]
 k_max = arr1[-k]
 print(f"{k}th min element:- ", k_min)
 print(f"{k}th max element:- ", k_max)
+
+
+
+# if list contains duplicates 
+
+k = int(input("enter the value of k:- "))
+arr1 = [-3, 6, 8, -3, 7, 100, 120,100,7, -300]
+
+arr2 = []
+
+for i in arr1:
+    if i not in arr2:
+        arr2.append(i)
+ 
+arr2.sort()
+print("min_num", arr2[k-1], "\n", "max_num", arr2[-k])
