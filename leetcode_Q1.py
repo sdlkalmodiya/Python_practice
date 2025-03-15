1a9e44656a51d6d4bb9864bd991b792727f60f18
sdlkalmodiya <101276375+sdlkalmodiya@users.noreply.github.com>
2025-03-16 01:47:24 +0530

    2 pointers method

--- leetcode_Q1.py
+++ leetcode_Q1.py
@@ -7,4 +7,19 @@ class Solution(object):
                 if nums[i]+nums[j] == target:
                     return [i, j]
 
-              
+ """ its time complexity is high o(n^2) as we are iterating through complete list for each element"""
+             
+# if sorted array is given then we can use two pointer method
+
+""" its time complexity is only o(n) as we are iterating once only through list"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        left,right = 0, len(nums)-1
+        while(left<right):
+            if nums[left]+nums[right] == target:
+                return [left, right]
+            elif nums[left]+nums[right] > target:
+                right -= 1
+            elif nums[left]+nums[right] < target:
+                left +=1
