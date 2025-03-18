da7772191984812debed70be9e17e2b270fb81ca
sdlkalmodiya <101276375+sdlkalmodiya@users.noreply.github.com>
2025-03-18 22:18:15 +0530

    Create leetcode_Q66.py

---
+++ leetcode_Q66.py
@@ -0,0 +1,14 @@
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+# Increment the large integer by one and return the resulting array of digits.
+
+class Solution(object):
+    def plusOne(self, digits):
+        last = len(digits)-1
+        for i in range(last, -1, -1):
+            if digits[i]<9:
+                digits[i] = digits[i]+1
+                return digits
+            digits[i] = 0
+
+        return [1]+[0]*len(digits)
